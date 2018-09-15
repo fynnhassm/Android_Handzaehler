f25e42ee26ccc701e2a6925967fa606a17a47c56
mide42 <demi_05@arcor.de>
2018-09-15 21:54:24 +0200

    Erste Version der App fertig samt README-Datei

---
+++ README.md
@@ -0,0 +1,24 @@
+# Android-App "Handzähler"
+
+App represents a simple [tally counter](https://en.wikipedia.org/wiki/Tally_counter).
+The purpose of the app is it to demonstrate event-handling for buttons.
+
+<br>
+
+Identifiers (names for classes, variables and methods) and UI texts are in German only.
+
+<br>
+
+----
+## Screenshots
+
+![Screenshot 1](screenshot_1.png)  ![Screenshot 2](screenshot_2.png)
+
+
+
+<br>
+
+----
+# License
+
+See the [LICENSE file](LICENSE.md) for license rights and limitations (BSD 3-Clause License).
---
+++ app/src/main/java/de/mide/handzaehler/MainActivity.java
@@ -0,0 +1,76 @@
+package de.mide.handzaehler;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+/**
+ * App represents a simple tally counter.
+ * <br><br>
+ *
+ * This project is licensed under the terms of the BSD 3-Clause License.
+ */
+public class MainActivity extends Activity
+                          implements View.OnClickListener {
+
+    /** Enthält immer aktuellen Zähler-Wert. */
+    protected int _zahl = 0;
+
+    /** Button zum Erhöhen des Zählers (+1). */
+    protected Button _plusButton  = null;
+
+    /** Button zum Erniedrigen des Zählers (-1). */
+    protected Button _minusButton = null;
+
+    /** UI-Element für Darstellung aktueller Zähler-Wert. */
+    protected TextView _zahlTextView = null;
+
+
+    /**
+     * Lifecycle-Methode. Lädt Layout-Datei, füllt Member-Variablen
+     * mit Referenzen auf UI-Elemente, und setzt Event-Handler-Objekt
+     * für Buttons.
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        _plusButton  = findViewById(R.id.plusButton);
+        _minusButton = findViewById(R.id.minusButton);
+
+        _zahlTextView = findViewById(R.id.zahlTextView);
+
+        _plusButton.setOnClickListener (this);
+        _minusButton.setOnClickListener(this);
+    }
+
+
+    /**
+     * Event-Handler-Methode für die beiden Buttons.
+     *
+     * @param v Referenz auf UI-Element, das das Event ausgelöst hat.
+     */
+    @Override
+    public void onClick(View v) {
+
+        if (v == _plusButton) {
+            _zahl++;
+        }
+        else if (v == _minusButton) {
+            _zahl--;
+        } else {
+            return; // Event ignorieren, weil von unbekanntem UI-Element ausgelöst
+        }
+
+        String anzeigeText = _zahl + "";      // Trick: int nach String wandeln
+        _zahlTextView.setText( anzeigeText );
+    }
+
+}
+
+
+
