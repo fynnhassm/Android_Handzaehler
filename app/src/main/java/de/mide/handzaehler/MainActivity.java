a8ef5bd702cd969d7ef269c24d04e7798d536f5c
MDecker-MobileComputing <mide_42b@yahoo.de>
2022-09-04 16:54:51 +0200

    Update auf Android Studio 2021.2.1 Patch 2

--- app/src/main/java/de/mide/handzaehler/MainActivity.java
+++ app/src/main/java/de/mide/handzaehler/MainActivity.java
@@ -1,41 +1,22 @@
 package de.mide.handzaehler;
 
-import android.app.Activity;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-/**
- * App repräsentiert einen einfachen Handzähler (Tally Counter). Hiermit
- * sollen die Grundlagen des Event-Handlings für UI-Elemente demonstriert
- * werden.
- * <br><br>
- *
- * This project is licensed under the terms of the BSD 3-Clause License.
- */
-public class MainActivity extends Activity
-                          implements View.OnClickListener {
-
-    /** Enthält immer aktuellen Zähler-Wert. */
     protected int _zahl = 0;
 
-    /** Button zum Erhöhen des Zählers (+1). */
     protected Button _plusButton  = null;
 
-    /** Button zum Erniedrigen des Zählers (-1). */
     protected Button _minusButton = null;
 
-    /** UI-Element für Darstellung aktueller Zähler-Wert. */
     protected TextView _zahlTextView = null;
 
-
-    /**
-     * Lifecycle-Methode. Lädt Layout-Datei, füllt Member-Variablen
-     * mit Referenzen auf UI-Elemente, und setzt Event-Handler-Objekt
-     * für die beiden Buttons.
-     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -51,7 +32,6 @@ public class MainActivity extends Activity
         _minusButton.setOnClickListener(this);
     }
 
-
     /**
      * Event-Handler-Methode für die beiden Buttons.
      *
@@ -63,8 +43,8 @@ public class MainActivity extends Activity
         if (view == _plusButton) {
 
             _zahl++;
-        }
-        else if (view == _minusButton) {
+
+        } else if (view == _minusButton) {
 
             _zahl--;
 
@@ -73,7 +53,7 @@ public class MainActivity extends Activity
             return; // Event ignorieren, weil von unbekanntem UI-Element ausgelöst
         }
 
-        String anzeigeText = _zahl + ""; // Trick: int nach String wandeln
+        String anzeigeText = _zahl + ""; // Trick: int nach String umwandeln
         _zahlTextView.setText( anzeigeText );
     }
 
